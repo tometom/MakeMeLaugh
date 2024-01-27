ecfa26a4ee55a06fa19cf38040785a38d59f913e
tometom <tombsantos@gmail.com>
2024-01-27 15:26:57 +0000

    beatTimer started

---
+++ Assets/Scripts/beatTimer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beatTimer : MonoBehaviour
+{
+    public Vector3 lclEndingPos,lclStartingPos;
+    Transform beatLine;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
