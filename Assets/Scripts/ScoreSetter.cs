e172bbe3a175bf342d5b9935b7e78bd395615faa
dCastanho <dc@cob.pt>
2024-01-28 15:14:57 +0000

    Score UI

--- Assets/Scripts/ScoreSetter.cs
+++ Assets/Scripts/ScoreSetter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,15 +9,10 @@ public class ScoreSetter : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = beatManager.totalScore.ToString();        
+        scoreText.text = Math.Ceiling(beatManager.totalScore).ToString();        
     }
 }
